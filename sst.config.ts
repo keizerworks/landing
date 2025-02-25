f04f66e723dcf1b6a2648fec9eacca22bf7d7a61
Sudarsh1010 <sudarshjain1010@gmail.com>
2025-02-25 12:22:06 +0530

    add sst & github workflows

---
+++ sst.config.ts
@@ -0,0 +1,29 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "keizerworks",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.Nextjs("www", {
+      domain:
+        $app.stage === "production"
+          ? {
+              name: "keizerworks.com",
+              redirects: ["www.keizerworks.com"],
+              dns: sst.cloudflare.dns(),
+            }
+          : undefined,
+      environment: {
+        EMAIL: process.env.EMAIL!,
+        EMAIL_APP_PASSWORD: process.env.EMAIL_APP_PASSWORD!,
+        NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL!,
+      },
+    });
+  },
+});
